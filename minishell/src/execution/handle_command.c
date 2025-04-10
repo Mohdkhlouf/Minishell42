87414319937aab34b045e10b2f144112795deb05
mohdkhlouf <mohammad.khlouf@gmail.com>
2025-04-10 15:54:35 +0300

    fixing files

--- minishell/src/execution/handle_command.c
+++ minishell/src/execution/handle_command.c
@@ -4,6 +4,33 @@
 then do the command execution*/
 
 
+void exec_cmd(t_cmds cmd, t_data *data)
+{
+	(void)data;
+	(void)cmd;
+	char *path;
+
+	path = NULL;
+	path = find_path(data, cmd.cmd[0]);
+	if (execve(path, cmd.cmd, NULL) == -1)
+		exit(1);
+}
+
+/* this function will start the fork to execute the cmd
+i did the fork here.
+then send the execution to child process*/
+int	execute_cmd(t_cmds cmd, t_data *data)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+		print_error("ERROR IN FORKING\n");
+	if (pid == 0)
+		exec_cmd(cmd, data);
+	return (pid);
+}
+
 /*this function recieved a cmd struct, first i check if the command is empty
 then if not, check if it is a built in, then will execute as built in.
 if not, i use another function to start executing the external cmd*/
@@ -12,7 +39,6 @@ void	handle_command(t_cmds cmd, t_data *data)
 	int	ret;
 	int pid[1];
 	int status;
-
 
 	ret = 0;
 	(void)data;
--- minishell/src/execution/handle_command_utils.c
+++ minishell/src/execution/handle_command_utils.c
@@ -39,32 +39,6 @@ char	*find_path(t_data *data, char *cmd)
 	return (NULL);
 }
 
-void exec_cmd(t_cmds cmd, t_data *data)
-{
-	(void)data;
-	(void)cmd;
-	char *path;
-
-	path = NULL;
-	path = find_path(data, cmd.cmd[0]);
-	if (execve(path, cmd.cmd, NULL) == -1)
-		exit(1);
-}
-
-/* this function will start the fork to execute the cmd
-i did the fork here.
-then send the execution to child process*/
-int	execute_cmd(t_cmds cmd, t_data *data)
-{
-	pid_t	pid;
-
-	pid = fork();
-	if (pid == -1)
-		print_error("ERROR IN FORKING\n");
-	if (pid == 0)
-		exec_cmd(cmd, data);
-	return (pid);
-}
 /*to handlle any empty cmd*/
 void	handle_empty_cmd(t_cmds cmd, t_data *data)
 {
@@ -72,3 +46,7 @@ void	handle_empty_cmd(t_cmds cmd, t_data *data)
 	(void)cmd;
 	printf("empty cmd handler\n");
 }
+
+
+
+
