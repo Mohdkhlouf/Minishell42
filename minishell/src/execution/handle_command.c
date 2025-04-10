b504f1d7488bb0311eb60a8c05c52f9892ac2316
mohdkhlouf <mohammad.khlouf@gmail.com>
2025-04-10 14:24:54 +0300

    adding execute function

--- minishell/src/execution/handle_command.c
+++ minishell/src/execution/handle_command.c
@@ -3,16 +3,61 @@
 /* main function to execute one command, i will make it execute the redirections
 then do the command execution*/
 
+char *join_cmd_with_slash(char *cmd)
+{
+	char *result;
+
+	result = NULL;
+	result = ft_strjoin("/", cmd);
+	if(!result)
+		print_error("Join is not done\n");
+	return (result);
+}
+
+char	*find_path(t_data *data, char *cmd)
+{
+	int		i;
+	char	*file_name;
+	int		not_found;
+
+	not_found = 0;
+	i = 0;
+	file_name = NULL;
+	cmd = join_cmd_with_slash(cmd);
+	while (data->parsed_path[i])
+	{
+		if (i != 0)
+			free(file_name);
+		file_name = ft_strjoin(data->parsed_path[i], cmd);
+		if (!file_name)
+			print_error("Join is not done\n");
+		if ((access(file_name, F_OK) == 0))
+			return (file_name);
+		else
+			not_found = 1;
+		i++;
+	}
+	if (not_found == 1)
+		print_error("FILE NOT FOUNT\n");
+	return (NULL);
+}
+
 void exec_cmd(t_cmds cmd, t_data *data)
 {
 	(void)data;
 	(void)cmd;
-	// if (execve("pipex->cmds[0].path", cmd.cmd, data->env_lst) == -1)
-	// 	exit(1);
+	char *path;
+
+	path = NULL;
+	path = find_path(data, cmd.cmd[0]);
+	if (execve(path, cmd.cmd, NULL) == -1)
+		exit(1);
 }
 
-
-void	execute_cmd(t_cmds cmd, t_data *data)
+/* this function will start the fork to execute the cmd
+i did the fork here.
+then send the execution to child process*/
+int	execute_cmd(t_cmds cmd, t_data *data)
 {
 	pid_t	pid;
 
@@ -21,9 +66,9 @@ void	execute_cmd(t_cmds cmd, t_data *data)
 		print_error("ERROR IN FORKING\n");
 	if (pid == 0)
 		exec_cmd(cmd, data);
-	
+	return (pid);
 }
-
+/*to handlle any empty cmd*/
 void	handle_empty_cmd(t_cmds cmd, t_data *data)
 {
 	(void)data;
@@ -31,9 +76,15 @@ void	handle_empty_cmd(t_cmds cmd, t_data *data)
 	printf("empty cmd handler\n");
 }
 
+/*this function recieved a cmd struct, first i check if the command is empty
+then if not, check if it is a built in, then will execute as built in.
+if not, i use another function to start executing the external cmd*/
 void	handle_command(t_cmds cmd, t_data *data)
 {
 	int	ret;
+	int pid[1];
+	int status;
+
 
 	ret = 0;
 	(void)data;
@@ -45,8 +96,9 @@ void	handle_command(t_cmds cmd, t_data *data)
 			printf("execute builtin \n");
 		}
 		else
-			execute_cmd(cmd, data);
+			pid[0] = execute_cmd(cmd, data);
 	}
 	else
 		handle_empty_cmd(cmd, data);
+	waitpid(pid[0], &status, 0);
 }
