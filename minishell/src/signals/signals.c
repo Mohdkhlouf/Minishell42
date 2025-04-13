f21505ab17f0e3ded6cc971061a447011f1337b0
mohdkhlouf <mohammad.khlouf@gmail.com>
2025-04-13 16:17:23 +0300

    fix signals

--- minishell/src/signals/signals.c
+++ minishell/src/signals/signals.c
@@ -16,16 +16,21 @@ void	handler(int num)
 	rl_redisplay();
 }
 /* Signals must be handled in 3 positions and modes,
-first: when nothing input. it will make new line.
-second: with processing child, or something is written in the line.
-thirsd: from << herdoc, EOF signal*/
+first: when nothing input. ctrl+c & ctr + \,  will make new line.
+second: something is written in the line, ctrl+c will do new line, ctr+\
+will be ignored, ctr+d will be ignored.
+third: when processing, execve.
+last: from << herdoc, EOF signal*/
 void	start_signal(void)
 {
 	struct sigaction	sa;
-
 	sa.sa_handler = handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
 	sigaction(SIGINT, &sa, NULL);
+	
+	// this will ignore the crtrl + \ like bash
+	sa.sa_handler = SIG_IGN;
 	sigaction(SIGQUIT, &sa, NULL);
+	
 }
