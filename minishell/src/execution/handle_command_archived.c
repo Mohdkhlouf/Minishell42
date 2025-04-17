cdcc752f8d7c4e61469a8c8d9857e1203ba08738
mohdkhlouf <mohammad.khlouf@gmail.com>
2025-04-17 10:36:10 +0300

    fixing 2 forking

--- minishell/src/execution/handle_command_archived.c
+++ minishell/src/execution/handle_command_archived.c
@@ -34,21 +34,21 @@ void exec_cmd(t_cmds *cmd, t_data *data)
 /* this function will start the fork to execute the cmd
 i did the fork here.
 then send the execution to child process*/
-// int	execute_cmd(t_cmds *cmd, t_data *data)
-// {
-// 	pid_t	pid;
+int	execute_cmd(t_cmds *cmd, t_data *data)
+{
+	pid_t	pid;
 
-// 	pid = fork();
-// 	if (pid == -1)
-// 	{
-// 		print_error("ERROR IN FORKING\n");
-// 		exit(1);
-// 	}
+	pid = fork();
+	if (pid == -1)
+	{
+		print_error("ERROR IN FORKING\n");
+		exit(1);
+	}
 		
-// 	if (pid == 0)
-// 		exec_cmd(cmd, data);
-// 	return (pid);
-// }
+	if (pid == 0)
+		exec_cmd(cmd, data);
+	return (pid);
+}
 
 /*this function recieved a cmd struct, first i check if the command is empty
 then if not, check if it is a built in, then will execute as built in.
@@ -56,12 +56,12 @@ if not, i use another function to start executing the external cmd*/
 void	handle_command(t_cmds *cmd, t_data *data)
 {
 	int	ret;
-	// int pid;
-	// int status;
+	int pid;
+	int status;
 
 	ret = 0;
-	// pid = 0;
-	// (void)data;
+	pid = 0;
+	(void)data;
 	if (!is_empty_cmd(cmd))
 	{
 		if (is_builtin(cmd->cmd[0]) == 1)
@@ -75,9 +75,8 @@ void	handle_command(t_cmds *cmd, t_data *data)
 		else
 		{
 			execute_redirections(data, cmd);
-			// pid = execute_cmd(cmd, data);
-			exec_cmd(cmd, data);
-			// waitpid(pid, &status, 0);
+			pid = execute_cmd(cmd, data);
+			waitpid(pid, &status, 0);
 		}
 	}
 	else
