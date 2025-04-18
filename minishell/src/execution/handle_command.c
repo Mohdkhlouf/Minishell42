c0dd2a477ee3ad44d120e744aebf1c01791ac6d7
mohdkhlouf <mohammad.khlouf@gmail.com>
2025-04-18 16:23:21 +0300

    push back

--- minishell/src/execution/handle_command.c
+++ minishell/src/execution/handle_command.c
@@ -1,52 +1,54 @@
 #include "../includes/minishell.h"
 
 /*reset the signals from child process*/
-void	set_default_signal_handlers(void)
+void set_default_signal_handlers(void)
 {
-	struct sigaction	sa;
+    struct sigaction sa;
 
-	sa.sa_handler = SIG_DFL;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sigaction(SIGINT, &sa, NULL);
-	sigaction(SIGQUIT, &sa, NULL);
+    sa.sa_handler = SIG_DFL;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGQUIT, &sa, NULL);
 }
+
 
 /* main function to execute one command, i will make it execute the redirections
 then do the command execution*/
-void	exec_cmd(t_cmds *cmd, t_data *data)
+void exec_cmd(t_cmds *cmd, t_data *data)
 {
-	char	*path;
-
 	(void)data;
+	char *path;
+
 	path = NULL;
 	set_default_signal_handlers();
 	path = find_path(data, cmd->cmd[0]);
-	if (!path)
-		return ;
+	if(!path)
+		return;
 	if (execve(path, cmd->cmd, data->envp) == -1)
-		return ;
+		return;
 	free(path);
 }
 
 /* this function will start the fork to execute the cmd
 i did the fork here.
 then send the execution to child process*/
-// int	execute_cmd(t_cmds *cmd, t_data *data)
-// {
-// 	pid_t	pid;
+int	execute_cmd(t_cmds *cmd, t_data *data)
+{
+	pid_t	pid;
 
-// 	pid = fork();
-// 	if (pid == -1)
-// 	{
-// 		print_error("ERROR IN FORKING\n");
-// 		exit(1);
-// 	}
-
-// 	if (pid == 0)
-// 		exec_cmd(cmd, data);
-// 	return (pid);
-// }
+	pid = fork();
+	if (pid == -1)
+	{
+		print_error("ERROR IN FORKING\n");
+		exit(1);
+	}
+		
+	if (pid == 0)
+		exec_cmd(cmd, data);
+	return (pid);
+}
 
 /*this function recieved a cmd struct, first i check if the command is empty
 then if not, check if it is a built in, then will execute as built in.
@@ -54,25 +56,30 @@ if not, i use another function to start executing the external cmd*/
 void	handle_command(t_cmds *cmd, t_data *data)
 {
 	int	ret;
+	int pid;
+	int status;
 
 	ret = 0;
+	pid = 0;
+	(void)data;
 	if (!is_empty_cmd(cmd))
 	{
 		if (is_builtin(cmd->cmd[0]) == 1)
 		{
 			/*function to execute builtin function the sent will be a command*/
 			ret = execute_builtin(data, cmd);
-			if (ret == -1)
+			if(ret == -1)
 				print_error("Error.\n");
+			printf("execute builtin \n");
 		}
 		else
 		{
 			execute_redirections(data, cmd);
-			// pid = execute_cmd(cmd, data);
-			exec_cmd(cmd, data);
-			// waitpid(pid, &status, 0);
+			pid = execute_cmd(cmd, data);
+			waitpid(pid, &status, 0);
 		}
 	}
 	else
 		handle_empty_cmd(cmd, data);
+	
 }
