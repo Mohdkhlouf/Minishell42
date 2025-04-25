9fdc3ee963c7656b90cef79f5c8577db3ef0178c
Akancha Kumari <akxa75041@gmail.com>
2025-04-25 16:50:26 +0300

    changed dprintf to write

--- minishell/src/execution/execute_pipe.c
+++ minishell/src/execution/execute_pipe.c
@@ -14,13 +14,20 @@ void	execute_child(t_data *data, t_parsed_data *cmds_d, int i, int *prev_cmd,
 		dup2(pipe_fd[1], STDOUT_FILENO);
 		close(pipe_fd[1]);
 	}
+	else
+	{
+		close(pipe_fd[0]);  
+        close(pipe_fd[1]); 
+	}
 	handle_command(&cmds_d->cmds[i], data, exit_code);
 }
 
 void	execute_parent(int *prev_cmd, t_data *data, int i, int cmds_counter)
 {
 	if (*prev_cmd != -1)
+	{
 		close(*prev_cmd);
+	}
 	if (i < cmds_counter - 1)
 	{
 		close(data->pipe_fd[1]);
@@ -53,9 +60,9 @@ void	handle_pipes(t_data *data, t_parsed_data *cmds_d, int *exit_code)
 	int	status;
 	int	i;
 
-	// int	j;
+	int	j;
 	prev_cmd = -1;
-	// j = 0;
+	j = 0;
 	i = 0;
 	while (i < cmds_d->cmds_counter)
 	{
@@ -63,24 +70,29 @@ void	handle_pipes(t_data *data, t_parsed_data *cmds_d, int *exit_code)
 		i++;
 	}
 	if (prev_cmd != -1)
+	{
 		close(prev_cmd);
-	// while (j < cmds_d->cmds_counter)
-	// {
-	waitpid(data->pid, &status, 0);
-	if (WIFEXITED(status))
-	{
-		*exit_code = WEXITSTATUS(status);
-		printf("Child process exited with code %d\n", *exit_code);
 	}
-	else if(WIFSIGNALED(status))
+
+	while (j < cmds_d->cmds_counter)
 	{
-		int signal_num = WTERMSIG(status);
-		printf("Child process terminated by signal %d\n", signal_num);
-	}
-	else if (WIFSTOPPED(status)) {
-		// Child process stopped
-		int stop_signal = WSTOPSIG(status);
-		printf("Child process stopped by signal %d\n", stop_signal);
+		waitpid(data->pid, &status, 0);
+		if (WIFEXITED(status))
+		{
+			*exit_code = WEXITSTATUS(status);
+			printf("Child process exited with code %d\n", *exit_code);
+		}
+		else if(WIFSIGNALED(status))
+		{
+			int signal_num = WTERMSIG(status);
+			printf("Child process terminated by signal %d\n", signal_num);
+		}
+		else if (WIFSTOPPED(status)) {
+			// Child process stopped
+			int stop_signal = WSTOPSIG(status);
+			printf("Child process stopped by signal %d\n", stop_signal);
+		}
+		j++;
 	}
 }
 
